151acf4629b49715c25c880d6a66893e330ffee0
Sadesha Hansana <sadeshahansana5@gmail.com>
2026-03-09 16:00:17 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/shprofilterupdate">{file_name}</a>\n\n<b>⚜️ Powered By : <a href="https://t.me/shprofilterupdate">[ SH Pro Update ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/shprofilterupdate">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/shprofilterupdate">[ SH Pro Update ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
