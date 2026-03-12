d34a8d8c9ef37416de8b0dcd73fb87df2d1ea200
Sadesha Hansana <sadeshahansana5@gmail.com>
2026-03-12 20:16:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ from os import environ, getenv
 from Script import script
 
 # Utility functions
-id_pattern = re.compile(r'^.\d+$')
+id_pattern = re.compile(r'^-?\d+$')
 
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
